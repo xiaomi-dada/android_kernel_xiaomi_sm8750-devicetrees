c4114ee9e2f01bc2e7fe89d80c07b15acd0f4efd
chandu078 <chandudyavanapelli03@gmail.com>
2025-08-01 13:12:43 +0530

    qcom: camera: Import oplus changes from CPH2723_15.0.2.103(EX01)
    Change-Id: I2b188e235eb91dfb6d767d33fd90013ea106d4b1

--- qcom/camera/config/sun.mk
+++ qcom/camera/config/sun.mk
@@ -1,18 +1,25 @@
 dtbo-$(CONFIG_ARCH_SUN)   := sun-camera.dtbo
-dtbo-$(CONFIG_ARCH_SUN)   += sun-camera-sensor-mtp.dtbo \
-				sun-camera-sensor-rumi.dtbo \
-				sun-camera-sensor-cdp.dtbo  \
-				sun-camera-sensor-hdk.dtbo  \
-				sun-camera-sensor-qrd.dtbo
+# dtbo-$(CONFIG_ARCH_SUN)   += sun-camera-sensor-mtp.dtbo \
+# 				sun-camera-sensor-rumi.dtbo \
+# 				sun-camera-sensor-cdp.dtbo  \
+# 				sun-camera-sensor-hdk.dtbo  \
+# 				sun-camera-sensor-qrd.dtbo
 
-dtbo-$(CONFIG_ARCH_TUNA)  += tuna-camera.dtbo
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/dodge-camera-overlay-evb.dtbo \
+			     oplus/dodge-camera-overlay-T0.dtbo \
+			     oplus/dodge-camera-overlay-T1.dtbo \
 
-dtbo-$(CONFIG_ARCH_TUNA)  += tuna-camera-sensor-mtp.dtbo \
-				tuna-camera-sensor-cdp.dtbo \
-				tuna-camera-sensor-qrd.dtbo
 
-dtbo-$(CONFIG_ARCH_KERA)  += kera-camera.dtbo
-dtbo-$(CONFIG_ARCH_KERA)  += kera-camera-sensor-mtp.dtbo \
-				kera-camera-sensor-cdp.dtbo \
-				kera-camera-sensor-qrd.dtbo \
-				kera-camera-sensor-rcm.dtbo
+
+
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/erhai-camera-overlay-evb.dtbo \
+				 oplus/erhai-camera-overlay-T0.dtbo \
+
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/hummer-camera-overlay-evb.dtbo \
+			     oplus/hummer-camera-overlay-T0.dtbo \
+
+
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/pagani-camera-overlay-T0.dtbo \
+
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/paganiIn-camera-overlay-T0.dtbo \
+
