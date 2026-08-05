a8fe2eb0ef178ec64e7127d8ffc2c996318c411c
Bruno Martins <bgcngm@gmail.com>
2026-08-05 22:07:42 +0100

    Merge remote-tracking branch 'sm8750-modules/lineage-22.2' into HEAD
    623d33f1 Merge tag 'KERNEL.PLATFORM.4.0.r1-17300-kernel.0' of https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/devicetree into HEAD
    f34d74aa Merge tag 'WLAN.LA.1.0.r1-09600-pakala.0' of https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/wlan-devicetree into HEAD
    
     Conflicts:
    	BUILD.bazel
    	qcom/wlan/Kbuild
    
    Change-Id: I295a0be146cfcc45b419ef613425ec75b3c0d75c

---
+++ qcom/camera/config/sun.mk
@@ -0,0 +1,25 @@
+dtbo-$(CONFIG_ARCH_SUN)   := sun-camera.dtbo
+# dtbo-$(CONFIG_ARCH_SUN)   += sun-camera-sensor-mtp.dtbo \
+# 				sun-camera-sensor-rumi.dtbo \
+# 				sun-camera-sensor-cdp.dtbo  \
+# 				sun-camera-sensor-hdk.dtbo  \
+# 				sun-camera-sensor-qrd.dtbo
+
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/dodge-camera-overlay-evb.dtbo \
+			     oplus/dodge-camera-overlay-T0.dtbo \
+			     oplus/dodge-camera-overlay-T1.dtbo \
+
+
+
+
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/erhai-camera-overlay-evb.dtbo \
+
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/hummer-camera-overlay-evb.dtbo \
+			     oplus/hummer-camera-overlay-T0.dtbo \
+
+
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/pagani-camera-overlay-T0.dtbo \
+
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/paganiIn-camera-overlay-T0.dtbo \
+
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/ktm-camera-overlay-T0.dtbo \
